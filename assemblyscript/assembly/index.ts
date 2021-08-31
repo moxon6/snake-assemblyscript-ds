407a36a59e1b129552f6ba872b1795c72dbc77f9
Martin Moxon <moxon6@googlemail.com>
2021-08-31 17:43:32 +0000

    Add SCREEN_TOP constant

--- assemblyscript/assembly/index.ts
+++ assemblyscript/assembly/index.ts
@@ -2,21 +2,23 @@ import Apple from './apple';
 import * as nds from './nds';
 import { Snake, SnakeDirection } from './snake';
 
+const SCREEN_TOP = 0;
+
 export function start(): void {
 
     nds.consoleDemoInit();	
     nds.consoleClear();
-    nds.NF_Set2D(0,0);
+    nds.NF_Set2D(SCREEN_TOP,0);
     nds.NF_SetRootFolder("NITROFS");
     
     nds.NF_InitTiledBgBuffers();
-    nds.NF_InitTiledBgSys(0);
+    nds.NF_InitTiledBgSys(SCREEN_TOP);
     
     nds.NF_LoadTiledBg("backgrounds/bg", "bg", 256, 256);
     nds.NF_CreateTiledBg(0, 3, "bg");
     
     nds.NF_InitSpriteBuffers();
-    nds.NF_InitSpriteSys(0);
+    nds.NF_InitSpriteSys(SCREEN_TOP);
     
     nds.NF_LoadSpriteGfx("sprites/tile", 0, 8, 8); 
     nds.NF_LoadSpritePal("palettes/segment", 0);
@@ -24,13 +26,13 @@ export function start(): void {
     nds.NF_LoadSpritePal("palettes/segment_dropshadow", 2);
     nds.NF_LoadSpritePal("palettes/apple_dropshadow", 3);
     
-    nds.NF_VramSpriteGfx(0, 0, 0, true);
-    nds.NF_VramSpritePal(0, 0, 0);
-    nds.NF_VramSpritePal(0, 1, 1);
-    nds.NF_VramSpritePal(0, 2, 2);
-    nds.NF_VramSpritePal(0, 3, 3);
+    nds.NF_VramSpriteGfx(SCREEN_TOP, 0, 0, true);
+    nds.NF_VramSpritePal(SCREEN_TOP, 0, 0);
+    nds.NF_VramSpritePal(SCREEN_TOP, 1, 1);
+    nds.NF_VramSpritePal(SCREEN_TOP, 2, 2);
+    nds.NF_VramSpritePal(SCREEN_TOP, 3, 3);
     
-    nds.NF_InitTextSys(0);
+    nds.NF_InitTextSys(SCREEN_TOP);
     
     nds.NF_LoadTextFont("fonts/font", "default", 256, 256, 0);
     nds.NF_CreateTextLayer(0, 0, 0, "default");
@@ -66,7 +68,7 @@ export function start(): void {
                 frame = 0;
                 for (let i: i32 = 0; i < snake.segments.length; i++) {
                     const seg = snake.segments[i];
-                    nds.NF_CreateSprite(0, (i as u8) + 1, 0, 2, seg.x, seg.y);
+                    nds.NF_CreateSprite(SCREEN_TOP, (i as u8) + 1, 0, 2, seg.x, seg.y);
                 }
                 snake.advanceHead();
                 const head = snake.segments[0];
@@ -83,21 +85,21 @@ export function start(): void {
                         nds.println("Game Over. \nPress start to continue.");
                     }
                 }
-                nds.NF_ClearTextLayer(0, 0);
+                nds.NF_ClearTextLayer(SCREEN_TOP, 0);
                 nds.NF_WriteText(0, 0, 2, 2, `SCORE: ${score}`);
                 nds.NF_UpdateTextLayers();
-                nds.NF_CreateSprite(0, 0, 0, 1, apple.x, apple.y);
+                nds.NF_CreateSprite(SCREEN_TOP, 0, 0, 1, apple.x, apple.y);
             }
-            nds.NF_SpriteOamSet(0);
+            nds.NF_SpriteOamSet(SCREEN_TOP);
             nds.swiWaitForVBlank();
             nds.oamUpdate();
         } else {
             
             nds.scanKeys();
             if (nds.keysHeld() & nds.KEYS.START) {
-                nds.NF_DeleteSprite(0, 0);
+                nds.NF_DeleteSprite(SCREEN_TOP, 0);
                 for (let i: i32 = 0; i < snake.segments.length; i ++) {
-                    nds.NF_DeleteSprite(0, i + 1 as u8);
+                    nds.NF_DeleteSprite(SCREEN_TOP, i + 1 as u8);
                 }
                 snake = new Snake();
                 apple = new Apple();
@@ -106,7 +108,7 @@ export function start(): void {
                 done = false;
                 nds.consoleClear();
             }
-            nds.NF_SpriteOamSet(0);
+            nds.NF_SpriteOamSet(SCREEN_TOP);
             nds.swiWaitForVBlank();
             nds.oamUpdate();
         }
